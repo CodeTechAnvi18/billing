a2f111762fe6581866a9b6cb65bc4a0cd1c6dbbd
CodeTechAnvi18 <155050606+CodeTechAnvi18@users.noreply.github.com>
2024-03-04 14:45:07 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('cafe.mysql.database.azure.com','anvita','AN#123vita','cafe_billing_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('billing-cafe.mysql.database.azure.com','anvita','AN#123vita','cafe_billing_db')or die("Could not connect to mysql".mysqli_error($con));
